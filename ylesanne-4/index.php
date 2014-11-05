3c885719853783f37c878d7357387ea5e7b10165
eve-link <eve.link.1992@gmail.com>
2014-11-05 11:24:09 +0200

    ül4 - finished?

--- ylesanne-4/index.php
+++ ylesanne-4/index.php
@@ -1,12 +1,23 @@
+<?php 
+$name = "arvestus";
+$value = "Cookie on olemas";
+$expire = time() +20;
+
+	setcookie($name, $value, $expire);
+$arvestus = isset($_COOKIE["arvestus"]) ? $_COOKIE : "";
+if ($arvestus="arvestus" ) { echo $value ;} 
+else {echo "Cookie-t ei ole!" ;}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>II perioodi arvestustöö - Ülesanne 4 - Küpsised</title>
-  </head>
+<head>
+	<meta charset="UTF-8">
+	<title>II perioodi arvestustöö - Ülesanne 4 - Küpsised</title>
+</head>
 
-  <body>
-    <!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
-    <pre><?php print_r($_COOKIE); ?></pre>
-  </body>
+<body>
+	<!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
+	<pre><?php print_r($_COOKIE); ?></pre>
+</body>
 </html>
