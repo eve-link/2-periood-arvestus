e637b861762435e23db35ae37e5bf4f935c4a7db
eve-link <eve.link.1992@gmail.com>
2014-11-05 10:54:54 +0200

    ül1 - not completed, error

---
+++ ylesanne-1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>II perioodi arvestustöö - Ülesanne 1 - GET päring</title>
+</head>
+
+<body>
+
+	<?php 
+	$username = "Eve";
+	$age= 22;
+	?> 
+	
+	<a href="<?php echo $page_link . '?username=' . $username . '&age=' . $age; ?>">Result</a>
+
+</body>
+</html>
