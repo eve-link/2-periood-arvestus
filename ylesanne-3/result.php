4184dd015f13946693e9927b85c8fb59d141af12
eve-link <eve.link.1992@gmail.com>
2014-11-05 11:12:18 +0200

    ül3 - not completed, not working

--- ylesanne-3/result.php
+++ ylesanne-3/result.php
@@ -9,6 +9,24 @@
     <!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
     <pre><?php print_r($_POST); ?></pre>
 
+    <?php 
+
+ 	if ($username = $_POST["username"] && $_POST['sex']) {
+ 		echo "Tere  {$username}, Tundub et olete {$sex}";}
+
+	elseif ($username = "Puudub" && $_POST['sex']) {
+		echo "Palun minge tagasi ja sisestage nimi.";}
+
+	elseif ($sex = "Puudub" && $_POST['username']) {
+		echo "Palun minge tagasi ja valige sugu.";}
+
+	elseif ($sex = "Puudub" && $username = "Puudub") {
+		echo "Palun minge tagasi ja täitke väljad.";}
+		
+ 	elseif ($sex = "Puudub" && $username = "Puudub") {
+		echo "Tundub, et sattusid siia otseteed pidi.";}
+     ?>
+
     <div><a href="index.php">Mine tagasi</a></div>
   </body>
 </html>
